5b7d1fb5b3ba55abffb45ee8fde376015bcd5950
key.guan <key.guan@qq.com>
2017-06-24 15:23:13 +0800

    feat:Provision app

--- AsysKcode/app/src/main/java/k/demo/asys/activity/DemoActivity.java
+++ AsysKcode/app/src/main/java/k/demo/asys/activity/DemoActivity.java
@@ -1,6 +1,8 @@
 package k.demo.asys.activity;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import k.demo.asys.R;
@@ -12,8 +14,10 @@ public class DemoActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_demo);
+        PackageManager pm = getPackageManager();
+        ComponentName cn = new ComponentName(this, DemoActivity.class);
+        pm.setComponentEnabledSetting(cn, PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
 
     }
 }
