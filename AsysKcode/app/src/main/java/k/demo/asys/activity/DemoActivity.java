97806b77668d72291f9b7cb113daee78551c1fc9
key.guan <key.guan@qq.com>
2017-06-24 15:04:35 +0800

    feat:init

---
+++ AsysKcode/app/src/main/java/k/demo/asys/activity/DemoActivity.java
@@ -0,0 +1,19 @@
+package k.demo.asys.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import k.demo.asys.R;
+
+
+public class DemoActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_demo);
+
+    }
+}
