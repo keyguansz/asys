8fb294cb73ff926bf2b39eea05258fb33695ee8f
kgd <key.guan@qq.com>
2017-11-03 21:56:53 +0800

    delete safe char

--- packages/apps/SettingsAs/app/src/main/java/com/android/settings/kdeprecated.java
+++ packages/apps/SettingsAs/app/src/main/java/com/android/settings/kdeprecated.java
@@ -1,12 +1,6 @@
 package com.android.settings;
 
-/**
- * @author :key.guan
- * @package :com.android.settings
- * @date : 2017/4/2
- * Description:
- * Copyright (c) 2017. DJI All Rights Reserved.
- */
+
 
 public @interface kdeprecated{
 }
