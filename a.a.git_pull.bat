a9dacc00dcec83d9960827c61293801be1848f67
keyguan-l <key.guan@qq.com>
2020-01-01 10:20:47 +0800

    'auto-commit'

--- a.a.git_pull.bat
+++ a.a.git_pull.bat
@@ -1,5 +1,17 @@
 git status
 pause
+@SET /a tryCnt=0
+@SET /a MaxTryCnt=10
+:PULL_LOOP
+set /a tryCnt=%tryCnt%+1
+ECHO try # %tryCnt%
 git pull
+IF ERRORLEVEL == 0 (
+    ECHO "git pull success!!!"
+) ELSE (
+	IF %tryCnt% LEQ %MaxTryCnt% (
+		GOTO PULL_LOOP
+	)
+)
 pause
 exit
--- a.a.git_push.bat
+++ a.a.git_push.bat
@@ -2,6 +2,24 @@ git status
 pause
 git add --all
 git commit -m 'auto-commit'
+git status
+pause
+@SET /a tryCnt=0
+@SET /a MaxTryCnt=10
+:PUSH_LOOP
+set /a tryCnt=%tryCnt%+1
+ECHO try # %tryCnt%
 git push
+IF ERRORLEVEL == 0 (
+    ECHO "git push success!!!"
+) ELSE (
+	IF %tryCnt% LEQ %MaxTryCnt% (
+		GOTO PUSH_LOOP
+	)
+)
+pause
+exit
+
+
 pause
 exit
